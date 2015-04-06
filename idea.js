03f514beb1295a249fb28ee206f065d5b3a27f16
Karl-Ludwig Reinhard <reinhard@synyx.de>
2015-04-06 20:00:49 +0200

    initial commit

---
+++ idea.js
@@ -0,0 +1,19 @@
+function build(data, provider, request, model, kaese) {
+
+	interlink.prefill();
+}
+
+function interlrink(data, provider) {
+
+}
+
+Function.prototype.prefill = function(arguments) {
+// extract arguments of calle
+//
+// extract arguments of yourself
+//
+// take only the matching names
+// do it and fill it with it
+//
+// return it 
+}
