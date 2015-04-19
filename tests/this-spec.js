b112b64be84f6a3847afc70587581a34444ae484
K4L <kl.reinhard@googlemail.com>
2015-04-19 11:11:20 +0200

    cleanup

--- tests/this-spec.js
+++
@@ -1,9 +0,0 @@
-describe('this tests', function() {
-   it('should be accessable', function() {
-       var a = 123
-       function showMe() {
-           return a;
-       }
-       expect(showMe.bind(this)()).toBe(123)
-   })
-})
